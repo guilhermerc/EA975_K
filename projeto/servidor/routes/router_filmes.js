b1edfc7d84398a6b5830a142244dbb013a4bbd71
guilhermerc <guilherme.riciolic@gmail.com>
2019-07-06 13:49:20 -0300

    Comentário são adicionados através da requisição POST em /filmes/id/:id/comentario, onde ':id' é o id do filme a ser comentado.

--- projeto/servidor/routes/router_filmes.js
+++ projeto/servidor/routes/router_filmes.js
@@ -163,8 +163,8 @@ router.get('/id/:id', function(req, res, next) {
 // 'curl --header "Content-Type: application/json" -d "{\"username\":\"gabriel\
 // ",\"data\":\"22/22/2222\", \"comentario\":\"Achei um lixão\", \"nota\":\"20\"
 // }" http://localhost:3000/id/0'
-router.post('/id/:id', function(req, res, next) {
-	console.log("POST filmes/id/:id");
+router.post('/id/:id/comentario', function(req, res, next) {
+	console.log("POST filmes/id/:id/comentario");
 	var response = {
 		"houveErro":              	false,
 		"mensagemErro":           	"",
